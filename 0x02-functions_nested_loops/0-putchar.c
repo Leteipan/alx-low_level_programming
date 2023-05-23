7c691122afc0d9472d9d1fb11e0d41d574c27e8e
Leteipan <leteipanmontane45@gmail.com>
2023-05-23 06:38:11 -0700

    c function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,44 @@
 #include "main.h"
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Prints Holberton as a message.
- *
- * Return: Always 0 (Success)
+ * print_char - function declaration
+ * Description: print _putchar, has no arguements
+ * Return: has no return value
  */
+
+char print_char(void);
+
+/**
+ * main - entry point
+ * Description: calls print_char()
+ * Return: 0
+ */
+
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar('\n');
+	print_char();
 	return (0);
 }
 
+/**
+ * print_char - function definition
+ * Description: prints out '_putchar'
+ * Return: has no return value
+ */
+
+char print_char(void)
+{
+	char my_string_array[] = "_putchar\n";
+	int index = 0;
+
+	while (my_string_array[index] != '\0')
+	{
+		char letter = my_string_array[index];
+
+		putchar(letter);
+		index++;
+	}
+	return (0);
+}
