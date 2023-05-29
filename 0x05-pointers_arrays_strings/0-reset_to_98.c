cec6ad5e48f034cdd9109f04c878058186e32e15
Leteipan <leteipanmontane45@gmail.com>
2023-05-29 02:15:19 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function declaration
+ * @n: pointer integer
+ * Return: void
+ */
+
+void reset_to_98(int *n);
+
+/**
+ * reset_to_98 - function definition
+ * Description: resets value to 98
+ * @n: the pointer integer
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
