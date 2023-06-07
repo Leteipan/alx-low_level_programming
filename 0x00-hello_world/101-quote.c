73cc0b38d58c7a2150605644fe5fb5d41f3e3e51
Leteipan <leteipanmontane45@gmail.com>
2023-06-07 04:58:32 -0700

    C - Recursion

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
- * Main - Entry point
+ * Main-Entry point
  *
- * Return:1
+ * Return: 1
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,1 @@
-#include<stdio.h>
-/**
- * Main-Entry point
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-	
+
